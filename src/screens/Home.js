0a7adf11c63a0938ee993cde85386d1fa5494179
Michael <michaelolivares123@gmail.com>
2020-09-17 21:08:47 -0500

    connection api and show movies in the home page

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { Title } from 'react-native-paper';
+
+import { getNewsMoviesApi } from '../api/movies';
+import CarouselVertical from '../components/CarouselVertical';
 
 export default Home = () => {
+    const [ newMovies, setNewMovies ] = useState(null);
+
+    useEffect(() => {
+        getNewsMoviesApi().then((response) => {
+            setNewMovies(response.results)
+        })
+    },[]);
     return (
-        <View>
-            <Text>
-                Estamos en Home
-            </Text>
-        </View>
+        <ScrollView showsVerticalScrollIndicator={false}>
+            {newMovies && (
+                <View style={styles.news}>
+                    <Title style={styles.newsTitle}> News Movies</Title>
+                    <CarouselVertical  
+                        data={newMovies}
+                    />
+                </View>
+            )}
+        </ScrollView>
     );
 };
+
+const styles = StyleSheet.create({
+    news: {
+        marginVertical: 10
+    },
+    newsTitle:{
+        marginBottom: 15,
+        marginHorizontal: 20,
+        fontWeight: 'bold',
+        fontSize: 22,
+    }
+})
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,4 @@
+export const API_HOST = 'https://api.themoviedb.org/3';
 export const API_KEY = '393f8a43fdc4781f243eac74abf82577';
+export const LANG = 'es-ES';
+export const BASE_PATH_IMG = 'https://image.tmdb.org/t/p'
